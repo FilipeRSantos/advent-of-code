3cd8281e7a7e000e0b50c59096712e724baed737
Filipe Santos <fprssantos@gmail.com>
2023-12-05 20:26:17 -0300

    feat: day 5, part 2 completed

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -1,6 +1,6 @@
+use std::ops::Range;
 use std::str::FromStr;
 
-#[derive(PartialEq)]
 enum MapOf {
     SeedToSoil,
     SoilToFertilizer,
@@ -29,9 +29,9 @@ impl FromStr for MapOf {
 }
 
 struct MapData {
-    destination_start_range: usize,
-    source_start_range: usize,
-    range: usize
+    destination_start_range: u128,
+    source_start_range: u128,
+    range: u128
 }
 
 impl FromStr for MapData {
@@ -39,7 +39,7 @@ impl FromStr for MapData {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut seeds = s.split_ascii_whitespace()
-            .map(|data| data.parse::<usize>().expect("Should be a valid number"));
+            .map(|data| data.parse::<u128>().expect("Should be a valid number"));
 
         Ok(MapData{
             destination_start_range: seeds.next().unwrap(),
@@ -50,49 +50,47 @@ impl FromStr for MapData {
 }
 
 struct MapSection {
-    kind: MapOf,
     data: Vec<MapData>,
 }
 
 impl MapSection {
 
-    fn get_destination(&self, source: usize) -> usize {
+    fn get_source(&self, destination: u128) -> u128 {
 
         for data in &self.data {
-            let destination = data.get_destination_value(source);
+            let source = data.get_source_value(destination);
 
             if destination != source {
-                return destination;
+                return source;
             }
         }
 
-        source
+        destination
     }
-
 }
 
 struct Map {
     sections: Vec<MapSection>,
-    seeds: Vec<usize>,
+    range_seeds: Vec<Range<u128>>,
 }
 
 impl Map {
 
-    fn get_seed_destination(&self, target: MapOf) -> Vec<usize> {
-        self.seeds.iter().map(|seed| {
-            let mut last_source: usize = *seed;
+    fn get_closest_destination(&self) -> u128 {
 
-            for section in &self.sections {
-                last_source = section.get_destination(last_source);
+        for i in 0.. {
 
-                if section.kind == target {
-                    return last_source;
-                }
+            let mut current_destination = i;
+            for section in self.sections.iter().rev() {
+                current_destination = section.get_source(current_destination);
             }
 
-            last_source
-        })
-        .collect::<Vec<_>>()
+            if self.range_seeds.iter().any(|range| range.contains(&current_destination)) {
+                return i;
+            }
+        }
+
+        panic!("Should not reach here");
     }
 
 }
@@ -104,11 +102,16 @@ impl FromStr for Map {
         let mut lines = s.lines();
         let mut sections: Vec<MapSection> = vec!();
         let mut current_section: Option<MapSection> = None;
+        let mut range_seeds: Vec<Range<u128>> = vec!();
 
-        let seeds = lines.next()
+        let mut seeds = lines.next()
             .expect("Should not be empty")[6..].trim().split_ascii_whitespace()
-            .map(|seed| seed.parse::<usize>().expect("Should be a valid number"))
-            .collect::<Vec<_>>();
+            .map(|seed| seed.parse::<u128>().expect("Should be a valid number"));
+
+        while let Some(start_at) = seeds.next() {
+            let range = seeds.next().unwrap() - 1;
+            range_seeds.push(Range { start: start_at, end: start_at + range });
+        }
 
         for line in lines {
             if line.is_empty() {
@@ -118,10 +121,7 @@ impl FromStr for Map {
 
                 current_section = None;
             } else if current_section.is_none() {
-                let kind = line.parse::<MapOf>().expect("Should be a valid field");
-
                 current_section = Some(MapSection{
-                    kind,
                     data: vec![],
                 });
             } else {
@@ -136,21 +136,21 @@ impl FromStr for Map {
         }
 
         Ok(Map {
-            seeds,
+            range_seeds,
             sections
         })
     }
 }
 
 impl MapData {
-    fn get_destination_value(&self, source: usize) -> usize {
-        let overlaps = source >= self.source_start_range && source <= self.source_start_range + self.range;
+    fn get_source_value(&self, destination: u128) -> u128 {
+        let overlaps = destination >= self.destination_start_range && destination <= self.destination_start_range + self.range;
 
         if !overlaps {
-            source
+            destination
         } else {
-            let diff= source - self.source_start_range;
-            self.destination_start_range + diff
+            let diff= destination - self.destination_start_range;
+            self.source_start_range + diff
         }
     }
 }
@@ -160,9 +160,9 @@ fn main() -> Result<(), std::io::Error> {
     let input = include_str!("input.txt");
 
     let map = input.parse::<Map>().expect("Should be a valid input");
-    let locations = map.get_seed_destination(MapOf::HumidityToLocation);
+    let closest_location = map.get_closest_destination();
 
-    println!("{:?}", locations.iter().min());
+    println!("{:?}", closest_location);
 
     Ok(())
 }
