231175862d8df4b0a0a24cf76562c37263bf18a5
Filipe Santos <fprssantos@gmail.com>
2023-12-07 20:04:04 -0300

    refactor: cleaned up code a little bit

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -90,14 +90,7 @@ impl FromStr for PokerHand {
                     'K' => 13,
                     'Q' => 12,
                     'T' => 10,
-                    '9' => 9,
-                    '8' => 8,
-                    '7' => 7,
-                    '6' => 6,
-                    '5' => 5,
-                    '4' => 4,
-                    '3' => 3,
-                    '2' => 2,
+                    '2'..='9' => char.to_digit(10).expect("Should be a valid number") as usize,
                     'J' => 1,
                     _ => panic!("{} should not exist", char)
                 }
