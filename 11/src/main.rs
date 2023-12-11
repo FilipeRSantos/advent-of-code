10e34a89cf883437f63da4484f1fae3bf3d878af
Filipe Santos <fprssantos@gmail.com>
2023-12-11 15:46:38 -0300

    feat: day 11, part 1 completed

---
+++ 11/src/main.rs
@@ -0,0 +1,54 @@
+use std::cmp::{max, min};
+
+fn main() -> Result<(), std::io::Error> {
+
+    //parse galaxies
+    //collect all rows and columns without galaxies
+    //save it to offset calculations
+    //loop trough all galaxies and cal pos - pos
+
+    let input = include_str!("input.txt");
+    let mut galaxies = vec![];
+
+    for (y, line) in input.lines().enumerate() {
+        for (w, char) in line.chars().enumerate() {
+            if char == '#' {
+                galaxies.push((y, w));
+            }
+        }
+    }
+
+    let empty_rows =
+        (0..input.lines().count())
+            .filter(|row| !galaxies.iter().any(|galaxy| galaxy.0 == *row))
+            .collect::<Vec<_>>();
+
+    let empty_columns =
+        (0..input.lines().next().unwrap().len())
+            .filter(|column| !galaxies.iter().any(|galaxy| galaxy.1 == *column))
+            .collect::<Vec<_>>();
+
+    let mut acc = 0;
+    for (index, current) in galaxies.iter().enumerate() {
+        let other_galaxies = galaxies.iter().skip(index+1);
+        for (_, target) in other_galaxies.into_iter().enumerate() {
+            let start_row = min(current.0, target.0);
+            let start_column = min(current.1, target.1);
+            let end_row = max(current.0, target.0);
+            let end_column = max(current.1, target.1);
+
+            let offset_columns = empty_columns.iter()
+                .filter(|column| start_column <= **column && **column <= end_column).count();
+
+            let offset_rows = empty_rows.iter()
+                .filter(|row| start_row <= **row && **row <= end_row).count();
+
+            let distance = (end_row - start_row) + (end_column - start_column) + offset_columns + offset_rows;
+            acc += distance;
+        }
+    }
+
+    println!("{:?}", acc);
+
+    Ok(())
+}
